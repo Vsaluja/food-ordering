0654e23d0dac104a16707b91371c04c1860193a6
Vsaluja <vanshsaluja200@gmail.com>
2024-05-28 05:20:00 -0300

    UI changes

--- frontend/src/app/register/page.jsx
+++ frontend/src/app/register/page.jsx
@@ -53,21 +53,24 @@ const Register = () => {
 
 
     return (
-        <Container className={`flex flex-col flex-1 bg-white m-10 px-4 sm:px-10 rounded-xl`}>
-            <Navbar />
-            <div className='flex flex-1 my-10 lg:my-4'>
-                <form onSubmit={(e) => handleRegister(e)} className=' flex flex-col gap-6 bg-[#E7F0FD] max-h-[550px] w-full max-w-[400px]  mx-auto py-4 px-6 rounded'>
-                    <h2 className={`${lilita.className} font-bold text-[40px] font-sans mb-2 text-[#313043]`}>Sign Up</h2>
-                    <input className='border-2 p-2 rounded w-full' type="text" placeholder='First name' onChange={(e) => setFirstName(e.target.value)} />
-                    <input className='border-2 p-2 rounded w-full' type="text" placeholder='Last name' onChange={(e) => setLastName(e.target.value)} />
-                    <input className='border-2 p-2 rounded w-full' type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
-                    <input className='border-2 p-2 rounded w-full' type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
-                    <button className='self-start ms-4 py-2 px-6 bg-[#313043] rounded font-bold text-white' type="submit">Register</button>
-                    <div className='ms-4 flex flex-col text-center sm:flex-row gap-2 text-[#313043] font-bold'>
-                        Already a user ?
-                        <Link className='text-[#F77193]' href={`/login`} >Sign in here</Link>
-                    </div>
-                </form>
+        <Container className={``}>
+            <div className='flex flex-col flex-1 bg-white my-10 m-2 sm:m-10 px-4 sm:px-10 rounded-xl'>
+
+                <Navbar />
+                <div className='flex flex-1 my-10 lg:my-4'>
+                    <form onSubmit={(e) => handleRegister(e)} className=' flex flex-col gap-6 bg-[#E7F0FD] max-h-[550px] w-full max-w-[400px]  mx-auto py-4 px-6 rounded'>
+                        <h2 className={`${lilita.className} font-bold text-[40px] font-sans mb-2 text-[#313043]`}>Sign Up</h2>
+                        <input className='border-2 p-2 rounded w-full' type="text" placeholder='First name' onChange={(e) => setFirstName(e.target.value)} />
+                        <input className='border-2 p-2 rounded w-full' type="text" placeholder='Last name' onChange={(e) => setLastName(e.target.value)} />
+                        <input className='border-2 p-2 rounded w-full' type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+                        <input className='border-2 p-2 rounded w-full' type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+                        <button className='self-start ms-4 py-2 px-6 bg-[#313043] rounded font-bold text-white' type="submit">Register</button>
+                        <div className='ms-4 flex flex-col text-center sm:flex-row gap-2 text-[#313043] font-bold'>
+                            Already a user ?
+                            <Link className='text-[#F77193]' href={`/login`} >Sign in here</Link>
+                        </div>
+                    </form>
+                </div>
             </div>
         </Container>
     )
